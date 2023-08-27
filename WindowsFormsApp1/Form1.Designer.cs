7e5cb9158d019166ad415787f7565f9648ea13e0
Adavilson <adavilsonjunior8@gmail.com>
2023-08-27 20:34:49 -0300

    alpha

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-2, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(813, 454);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.panel2);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(805, 428);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Gainsboro;
+            this.button7.Location = new System.Drawing.Point(304, 324);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(463, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Gainsboro;
+            this.button6.Location = new System.Drawing.Point(304, 295);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(463, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gainsboro;
+            this.button5.Location = new System.Drawing.Point(304, 266);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(463, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gainsboro;
+            this.button4.Location = new System.Drawing.Point(304, 237);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(463, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3.Location = new System.Drawing.Point(304, 208);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(463, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.Location = new System.Drawing.Point(304, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(463, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Location = new System.Drawing.Point(304, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(463, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.btn_pause);
+            this.panel2.Location = new System.Drawing.Point(0, 83);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 345);
+            this.panel2.TabIndex = 1;
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.Location = new System.Drawing.Point(76, 212);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(40, 38);
+            this.btn_pause.TabIndex = 0;
+            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Firebrick;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(799, 85);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(484, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Super Music Player ♩ ♪ ♬ ♫";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(805, 428);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_pause;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using WMPLib;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        WMPLib.WindowsMediaPlayer ncb1 = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer ncb2 = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer ncb3 = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer ncb4 = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer ncb5 = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer ncb6 = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer ncb7 = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer CurrentMusic = new WMPLib.WindowsMediaPlayer();
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ncb1.URL= @"C:\Users\adavi\source\repos\WindowsFormsApp1\WindowsFormsApp1\Sounds\ncb1.mp3";
+            ncb1.controls.play();
+            CurrentMusic.controls.stop();
+            CurrentMusic = ncb1;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ncb2.URL = @"C:\Users\adavi\source\repos\WindowsFormsApp1\WindowsFormsApp1\Sounds\ncb2.mp3";
+            ncb2.controls.play();
+            CurrentMusic.controls.stop();
+            CurrentMusic = ncb2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ncb3.URL = @"C:\Users\adavi\source\repos\WindowsFormsApp1\WindowsFormsApp1\Sounds\ncb3.mp3";
+            ncb3.controls.play();
+            CurrentMusic.controls.stop();
+            CurrentMusic = ncb3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ncb4.URL = @"C:\Users\adavi\source\repos\WindowsFormsApp1\WindowsFormsApp1\Sounds\ncb4.mp3";
+            ncb4.controls.play();
+            CurrentMusic.controls.stop();
+            CurrentMusic = ncb4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ncb5.URL = @"C:\Users\adavi\source\repos\WindowsFormsApp1\WindowsFormsApp1\Sounds\ncb5.mp3";
+            ncb5.controls.play();
+            CurrentMusic.controls.stop();
+            CurrentMusic = ncb5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ncb6.URL = @"C:\Users\adavi\source\repos\WindowsFormsApp1\WindowsFormsApp1\Sounds\ncb6.mp3";
+            ncb6.controls.play();
+            CurrentMusic.controls.stop();
+            CurrentMusic = ncb6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ncb7.URL = @"C:\Users\adavi\source\repos\WindowsFormsApp1\WindowsFormsApp1\Sounds\ncb7.mp3";
+            ncb7.controls.play();
+            CurrentMusic.controls.stop();
+            CurrentMusic = ncb7;
+        }
+
+        bool IsPaused = false;
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            if (!IsPaused) { CurrentMusic.controls.pause(); IsPaused = true; }
+            else { CurrentMusic.controls.play(); IsPaused = false; }
+
+        }
+        
+        
+
+    }
+}
